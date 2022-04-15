268aba38162019574c8eea1cb5cfc9be1ad921ca
KojiroSchatten <banbirou@icloud.com>
2022-04-15 19:35:08 +0900

    feat: hooking up PrismaCLient to the resolver context

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from "apollo-server";
-
+import { context } from "./context";
 import { schema } from "./schema";
-export const server = new ApolloServer({ schema });
+export const server = new ApolloServer({ schema, context });
 
 const port = 3000;
 server.listen({ port }).then(({ url }) => {
--- src/schema.ts
+++ src/schema.ts
@@ -5,8 +5,15 @@ import * as types from "./graphql";
 export const schema = makeSchema({
   types,
   outputs: {
-  // nexusに.graphql拡張子にスキーマファイルを出力させる
-  schema: join(process.cwd(), "schema.graphql"),
-  // type definition
-  typegen: join(process.cwd(), "nexus-typegen.ts"),
-}})
+    // nexusに.graphql拡張子にスキーマファイルを出力させる
+    schema: join(process.cwd(), "schema.graphql"),
+    // type definition
+    typegen: join(process.cwd(), "nexus-typegen.ts"),
+  },
+  contextType: {
+    // contextインターフェースがexportされているファイルへのpath
+    module: join(process.cwd(), "./src/context.ts"),
+    // そのmodule内でexportされるインターフェースの名前
+    export: "Context",
+  }
+});
